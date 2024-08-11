b43531ce97a9d36ffb21cc9c1f7c135360ffde97
Siddharth <siddharth.pand8y@gmail.com>
2024-08-11 22:12:11 +0530

    feat: add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
-import * as path from 'path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
-import { fileURLToPath } from 'url'
+import * as path from "path";
+import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
+import { FastifyPluginAsync } from "fastify";
+import { fileURLToPath } from "url";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export type AppOptions = {
   // Place your custom options for app below here.
 } & Partial<AutoloadPluginOptions>;
 
-
 // Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {
-}
+const options: AppOptions = {};
 
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts
 ): Promise<void> => {
   // Place here your custom code!
 
@@ -27,20 +27,36 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // those should be support plugins that are reused
   // through your application
   void fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
+    dir: path.join(__dirname, "plugins"),
     options: opts,
-    forceESM: true
-  })
+    forceESM: true,
+  });
+
+  void fastify.register(fastifySwagger, {
+    swagger: {
+      info: {
+        title: "My Title",
+        description: "My Description.",
+        version: "1.0.0",
+      },
+      host: "localhost",
+      schemes: ["http", "https"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [{ name: "Default", description: "Default" }],
+    },
+  });
+
+  void fastify.register(fastifySwaggerUi);
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   void fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
+    dir: path.join(__dirname, "routes"),
     options: opts,
-    forceESM: true
-  })
-
+    forceESM: true,
+  });
 };
 
 export default app;
-export { app, options }
+export { app, options };
